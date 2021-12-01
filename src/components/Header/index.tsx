42e559c288d317a55eb6701b808013532b8f5199
huy <giahuy@redweb.dk>
2021-12-01 18:04:41 +0700

    up

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import { Row, Col, Divider, Avatar, Badge } from "antd";
+import avatar from "assets/images/Discord.png";
+import { Menus } from "./Menus";
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <Row>
+        <Col span={24}>
+          <h5>Tran Gia Huy</h5>
+          <Avatar
+            size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+            icon={<img src={avatar} alt="" />}
+          />
+          <p>
+            Hi, my name is Simon Doe and I'm a senior software engineer. Welcome
+            to my personal website!
+          </p>
+          <div className="social__list">
+            <Avatar className="m__x1" shape="circle" size="large" />
+
+            <Avatar className="m__x1" shape="circle" size="large" />
+
+            <Avatar className="m__x1" shape="circle" size="large" />
+
+            <Avatar className="m__x1" shape="circle" size="large" />
+
+            <Avatar className="m__x1" shape="circle" size="large" />
+          </div>
+        </Col>
+        <Divider orientation="left"></Divider>
+        <Col span={24}>
+          <Menus />
+        </Col>
+      </Row>
+    </header>
+  );
 };
--- src/layouts/Site/index.tsx
+++ src/layouts/Site/index.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Header } from "components/Header";
 import { Footer } from "components/Footer";
+import { Row, Col, Divider } from "antd";
+
 interface SiteProps {
   children: React.ReactNode;
 }
 
 export const Site: React.FC<SiteProps> = ({ children }) => {
   return (
-    <>
-      <Header />
-      <h1>Sites</h1>
-      {children}
-      <Footer />
-    </>
+    <Row>
+      <Col span={4}>
+        <Header />
+      </Col>
+      <Col span={20}>
+        <h1>Sites</h1>
+        {children}
+      </Col>
+
+      {/* <Footer /> */}
+    </Row>
   );
 };
